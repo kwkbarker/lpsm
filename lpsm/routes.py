707b0c06d6a3a3eb27be26761b81a4b3ec059eea
Kevin Barker <kwkbarker@gmail.com>
2021-11-16 21:23:25 -0500

    start of routes and templates

---
+++ lpsm/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template, request, url_for
+from werkzeug.utils import redirect
+
+from lpsm.config import KEY
+
+@app.route('/', methods=['get', 'post'])
+def landing():
+    if request.method == 'post':
+        if request.form.get('secret_key') == KEY:
+            return render_template('index.html')
+    return redirect(url_for('/'))
+
